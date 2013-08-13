da9c946b5253ac6b4e4771d894e14a0e9c0e92ad
Heehong Moon <bbirec@gmail.com>
2013-08-13 23:36:22 +0900

    Adding sprites and more helper functions.

--- src/clj_playn.clj
+++ src/clj_playn.clj
@@ -4,20 +4,60 @@
                        PlayN)
            (playn.java JavaPlatform)))
 
-(defonce layer (atom nil))
+;; Utils
+
+(defn assets []
+  (PlayN/assets))
+
+(defn graphics []
+  (PlayN/graphics))
+
+(defn root-layer []
+  (.rootLayer (graphics)))
+
+;;;; Sprites
+
+(defonce sprites (atom {}))
+
+(defn load-sprite [key file]
+  (when (nil? (get @sprites key))
+    (println (str "Load image : " file " as " key))
+    (swap! sprites
+           #(assoc %1 key
+                   (let [img (-> (assets)
+                                 (.getImage file))]
+                     (-> (graphics)
+                         (.createImageLayer img)))))))
+
+
+(defn add-sprite [key]
+  (let [layer (get @sprites key)]
+    (-> (root-layer)
+        (.add layer))))
+
+(defn get-sprite [key]
+  (get @sprites key))
+
+(defn set-translation [key x y]
+  (.setTranslation (get-sprite key) x y))
+
+
+;;;; Animation
+
+
+
+
+;;;; Entry point
 
 (defn -init []
-  (let [image (.getImage (PlayN/assets) "dog.jpg")
-        l (.createImageLayer (PlayN/graphics) image)]
-    (reset! layer l)
-    (.add (.rootLayer (PlayN/graphics)) l)))
-
+  (load-sprite :dog "dog.jpg")
+  (add-sprite :dog))
 
 (defn -update [delta]
   )
 
 (defn -paint [alpha]
-  (.setTranslation @layer 0 0))
+  )
 
 
 (defn -main [& args]
