744b0a4d8f1818d841e43a69eb02592b9b1bf7bf
Heehong Moon <bbirec@gmail.com>
2013-08-14 01:04:13 +0900

    Move animation.

--- src/clj_playn.clj
+++ src/clj_playn.clj
@@ -44,6 +44,22 @@
 
 ;;;; Animation
 
+(defonce anis (atom []))
+
+(defn move-sprite [key from to duration]
+  (swap! anis
+         #(conj %1
+                {:fn :move
+                 :key key
+                 :from from
+                 :to to
+                 :duration duration
+                 :elapsed 0
+
+                 :total-x (- (first to) (first from))
+                 :total-y (- (second to) (second from))
+                 :dx 0
+                 :dy 0})))
 
 
 
@@ -54,20 +70,56 @@
   (add-sprite :dog))
 
 (defn -update [delta]
-  )
+  ;; Delete finished animations
+  (swap!
+   anis
+   (fn [anis]
+     (filter #(not (>= (:elapsed %1)
+                       (:duration %1)))
+             anis)))
+
+  ;; Update animations
+  (let [dt (/ delta 1000)]
+   (swap!
+    anis
+    (fn [anis]
+      (map
+       (fn [a]
+         (let [duration (:duration a)
+               dx (/ (* dt (:total-x a)) duration)
+               dy (/ (* dt (:total-y a)) duration)]
+           
+           ;; Update values
+           (merge a
+                  {:elapsed (+ (:elapsed a) dt)
+                   :dx dx
+                   :dy dy})))
+       anis)))))
 
 (defn -paint [alpha]
-  )
+  (doseq [a @anis]
+    (let [sprite (get-sprite (:key a))
+          tx (.tx sprite)
+          ty (.ty sprite)
+          x (+ tx (:dx a))
+          y (+ ty (:dy a))]
+
+      (.setTx sprite x)
+      (.setTy sprite y))))
 
 
 (defn -main [& args]
   (let [platform (JavaPlatform/register)]
-    (future (PlayN/run
-      (proxy [Game$Default] [25]
-        (init []
-          (-init))
-        (update [delta]
-          (-update delta))
-        (paint [alpha]
-          (-paint alpha)))))))
+    (future
+      (try 
+       (PlayN/run
+        (proxy [Game$Default] [25]
+          (init []
+            (-init))
+          (update [delta]
+            (-update delta))
+          (paint [alpha]
+            (-paint alpha))))
+       (catch Exception e
+         (println (.getMessage e)))))))
 
